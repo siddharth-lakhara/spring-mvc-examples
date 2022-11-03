2d060cb49fded6ddedd1b2be1e388dd0ad84dfe2
lakhara <siddharth.lakhara@gmail.com>
2022-11-03 09:36:05 +0530

    initial commit

---
+++ src/test/java/com/lakhara/SpringMvcExample/SpringMvcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakhara.SpringMvcExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
